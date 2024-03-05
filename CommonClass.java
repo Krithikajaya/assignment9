7b4f6f1ad4475aab1355aa65e8b51c2bc50bf794
Krithikajaya <158828752+Krithikajaya@users.noreply.github.com>
2024-03-05 19:21:47 +0530

    Add files via upload

---
+++ CommonClass.java
@@ -0,0 +1,30 @@
+package assignment9;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class CommonClass {
+	public ChromeDriver driver;
+	@BeforeMethod
+	public void precondition() {
+		driver  = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();		
+	}
+	@AfterMethod
+	public void aftercondition() {
+		driver.close();
+	}
+
+}
+    
